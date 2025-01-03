b710e833603128b94206bfbce4573664615c7039
CasperRiis <7541911+CasperRiis@users.noreply.github.com>
2025-01-03 19:39:20 +0100

    invalidate books queries when opening homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookGrid from "../components/BookGrid";
 import GenreFilter from "../components/GenreFilter";
 import { Divider } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
 
 const genres = ["Fantasy", "SciFi", "Kids", "Teen", "Autobiography"];
 
 const HomePage = () => {
   const [genre, setGenre] = useState("");
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ["books"] });
+  }, [queryClient]);
+
   return (
     <>
       <GenreFilter genres={genres} onChange={setGenre} />
